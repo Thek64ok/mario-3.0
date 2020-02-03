59435450ec1a5e55a686ab4d9983cd7206e40ef4
Thek64ok <60596506+Thek64ok@users.noreply.github.com>
2020-02-03 18:47:18 +0200

    враги(тест)+система хп

---
+++ Mario 3.0/Assets/scripts/EnemyControll.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControll : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody2D myRigidbody;
+    private bool moving;
+    public float timeBetweenMove;
+    public float timeToMove;
+    private Vector3 moveDirection;
+    private float timeBetweenMoveCounter;
+    private float timeToMoveCounter;
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        timeBetweenMoveCounter = timeBetweenMove;
+        timeToMoveCounter = timeToMove; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moving)
+        {
+            timeToMoveCounter -= Time.deltaTime;
+            myRigidbody.velocity = moveDirection;
+            if (timeToMoveCounter < 0f)
+            {
+                moving = false;
+                timeBetweenMoveCounter = timeBetweenMove;
+            }
+        } else
+        {
+            timeBetweenMoveCounter -= Time.deltaTime;
+            myRigidbody.velocity = Vector2.zero;
+            if (timeBetweenMoveCounter < 0f)
+            {
+                moving = true;
+                timeToMoveCounter = timeToMove;
+                moveDirection = new Vector3(Random.Range(-1f, 1f) * moveSpeed, Random.Range(-1f, 1f) * moveSpeed, 0f);
+            }
+        }
+    }
+}
---
+++ Mario 3.0/Assets/scripts/Knight_HealthSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight_HealthSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int knightMaxHealth;
+    public int knightCurrentHealth;
+    void Start()
+    {
+        knightCurrentHealth = knightMaxHealth; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (knightCurrentHealth < 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void Heart(int damageToGive)
+    {
+        knightCurrentHealth -= damageToGive;
+    }
+    public void SetMaxHealth()
+    {
+        knightCurrentHealth = knightMaxHealth;
+    }
+}
