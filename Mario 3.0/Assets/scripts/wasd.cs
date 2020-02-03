078a52c70d8c38e510e96ff663eab0d412612e96
Thek64ok <60596506+Thek64ok@users.noreply.github.com>
2020-02-03 12:33:49 +0200

    knigh

---
+++ Mario 3.0/Assets/scripts/wasd.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wasd : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed = 20f;
+    private Rigidbody2D rb;
+    private Animator anime;
+
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody2D>();    
+        anime = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, 0f, 0f));
+        }
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * speed * Time.deltaTime, 0f));
+        }
+
+        anime.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anime.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+    }
+}
