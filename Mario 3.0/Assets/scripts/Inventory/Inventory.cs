577c7ca72b0dc3670ec35b03d18d32782f705ae6
twixer32 <60601115+twixer32@users.noreply.github.com>
2020-02-03 16:39:49 +0200

    Inventory 1.1

---
+++ Mario 3.0/Assets/scripts/Inventory/Inventory.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class Inventory : MonoBehaviour
+{
+    public List<ItemInentory> items = new List<ItemInentory> ();
+    public GameObject gameObjShow;
+    public GameObject InventoryMainObject;
+    public int maxCount;
+    public void addGraphics()
+    {
+        for(int i=0;i< maxCount;i++)
+        {
+            GameObject newItem = Instantiate(gameObjShow,InventoryMainObject.transform) as GameObject;
+            newItem.name = i.ToString();
+
+            ItemInentory ii = new ItemInentory();
+            ii.intemGameObj = newItem;
+            RectTransform rt = newItem.GetComponent<RectTransform>();
+
+            rt.localPosition = new Vector3(0, 0, 0);
+            rt.localScale = new Vector3(1, 1, 1);
+           
+            newItem.GetComponentInChildren<RectTransform>().localScale = new Vector3(1, 1, 1);
+            Button tempButton = newItem.GetComponent<Button>();
+            items.Add(ii);
+        }
+    }
+}
+[System.Serializable]
+public class ItemInentory
+{
+    public int id;
+    public GameObject intemGameObj;
+    public int count;
+}
