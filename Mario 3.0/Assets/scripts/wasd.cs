3522e3319d0706c44a6a1fd05c8e1d2efd8049e9
Thek64ok <60596506+Thek64ok@users.noreply.github.com>
2020-02-03 15:48:27 +0200

    анимация+камера

--- Mario 3.0/Assets/scripts/wasd.cs
+++ Mario 3.0/Assets/scripts/wasd.cs
@@ -9,6 +9,8 @@ public class wasd : MonoBehaviour
     public float speed = 20f;
     private Rigidbody2D rb;
     private Animator anime;
+    private bool moving;
+    private Vector2 lastMove;
 
     void Start()
     {
@@ -19,16 +21,24 @@ public class wasd : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        moving = false;
         if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
         {
             transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, 0f, 0f));
+            moving = true;
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
         }
         if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
         {
             transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * speed * Time.deltaTime, 0f));
+            moving = true;
+            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
         }
 
         anime.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
         anime.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        anime.SetBool("Moving", moving);
+        anime.SetFloat("LastMoveX", lastMove.x);
+        anime.SetFloat("LastMoveY", lastMove.y);
     }
 }
