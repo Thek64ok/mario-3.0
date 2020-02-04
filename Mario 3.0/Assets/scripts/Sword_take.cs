c80a1124cdb255e2cf8a319895e0897b3a14de82
Thek64ok <60596506+Thek64ok@users.noreply.github.com>
2020-02-04 14:07:45 +0200

    голый чел+подбирание меча(мнимое)

---
+++ Mario 3.0/Assets/scripts/Sword_take.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sword_take : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text pickUpText;
+    private bool pickUpAllowed;
+    void Start()
+    {
+        pickUpText.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pickUpAllowed && Input.GetKeyDown(KeyCode.E))
+            PickUp();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Knight"))
+        {
+            pickUpText.gameObject.SetActive(true);
+            pickUpAllowed = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Knight"))
+        {
+            pickUpText.gameObject.SetActive(false);
+            pickUpAllowed = false;
+        }
+    }
+    private void PickUp()
+    {
+        Destroy(gameObject);
+    }
+
+}
