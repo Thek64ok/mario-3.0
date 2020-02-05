773fa4426d766b7c90208f8ee5e6577bcd7b8aaa
Thek64ok <60596506+Thek64ok@users.noreply.github.com>
2020-02-06 00:01:14 +0200

    фулл новая анимация+атака+хп

--- Mario 3.0/Assets/scripts/wasd.cs
+++ Mario 3.0/Assets/scripts/wasd.cs
@@ -5,34 +5,76 @@ using UnityEngine;
 public class wasd : MonoBehaviour
 {
     public float speed = 20f;
+    private float currentMoveSpeed;
     private Rigidbody2D rb;
     private Animator anime;
     private bool moving;
     private Vector2 lastMove;
+    private Rigidbody2D myrigidbody2D;
     public int dayn;
     public GameObject Readl_Sword;
-    private bool ll;
+    private bool attacking;
+    public float attackTime;
+    private float attackTimeCounter;
 
     void Start()
     {   
         anime = GetComponent<Animator>();
         Readl_Sword.SetActive(false);
+        myrigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         moving = false;
-        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        if (!attacking)
         {
-            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, 0f, 0f));
-            moving = true;
-            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+            if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+            {
+                // transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, 0f, 0f));
+                myrigidbody2D.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * currentMoveSpeed, myrigidbody2D.velocity.y);
+                moving = true;
+                lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+            }
+            if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+            {
+                //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * speed * Time.deltaTime, 0f));
+                myrigidbody2D.velocity = new Vector2(myrigidbody2D.velocity.x, Input.GetAxisRaw("Vertical") * currentMoveSpeed);
+                moving = true;
+                lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+            }
+            if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+            {
+                myrigidbody2D.velocity = new Vector2(0f, myrigidbody2D.velocity.y);
+            }
+            if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+            {
+                myrigidbody2D.velocity = new Vector2(myrigidbody2D.velocity.x, 0f);
+            }
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                attackTimeCounter = attackTime;
+                attacking = true;
+                myrigidbody2D.velocity = Vector2.zero;
+                anime.SetBool("Attack", true);
+            }
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.5f && Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0.5f)
+            {
+                currentMoveSpeed = speed / 1.5f;
+            }
+            else
+            {
+                currentMoveSpeed = speed;
+            }
         }
-        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        if (attackTimeCounter > 0)
         {
-            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * speed * Time.deltaTime, 0f));
-            moving = true;
-            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+            attackTimeCounter -= Time.deltaTime * 5;
+        }
+        if (attackTimeCounter <= 0)
+        {
+            attacking = false;
+            anime.SetBool("Attack", false);
         }
         anime.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
         anime.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
@@ -43,19 +85,6 @@ public class wasd : MonoBehaviour
         {
             anime.SetTrigger("taking");
             Readl_Sword.SetActive(true);
-        }
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            ll = true;
-            anime.SetBool("Attack", ll);
-        }
-        else
-        {
-            if (Input.GetKeyDown(KeyCode.B))
-            {
-                ll = false;
-                anime.SetBool("Attack", ll);
-            }
         }
     }
 }
