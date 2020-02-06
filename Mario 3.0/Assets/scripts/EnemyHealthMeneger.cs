cebd19c3ebd36dbfcf256c85c53ce0519a988164
Thek64ok <60596506+Thek64ok@users.noreply.github.com>
2020-02-06 14:22:51 +0200

    хп враги

---
+++ Mario 3.0/Assets/scripts/EnemyHealthMeneger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthMeneger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int MaxHealth;
+    public int CurrentHealth;
+  //  public Texture2D texHealth;
+    void Start()
+    {
+        CurrentHealth = MaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CurrentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    /*
+    void OnGUI()
+    {
+        GUI.Box(new Rect(600, Screen.height - 200, (knightMaxHealth * 10) / 2, 10), "");
+        GUI.DrawTexture(new Rect(600, Screen.height - 200, (knightCurrentHealth * 10) / 2, 10), texHealth);
+    }
+    */
+    public void HurtEnemy(int damageToGive)
+    {
+        CurrentHealth -= damageToGive;
+    }
+    public void SetMaxHealth()
+    {
+       CurrentHealth = MaxHealth;
+    }
+}
