b27255987b19cc837bae72f00903131aab9bb3a7
Thek64ok <60596506+Thek64ok@users.noreply.github.com>
2020-02-10 23:31:27 +0200

    почти сделал анимации мобов

---
+++ Mario 3.0/Assets/scripts/FromUnder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FromUnder : MonoBehaviour
+{
+    public GameObject Spider;
+    public Animator anim;
+    private Transform Knight;
+    public float TimeOf;
+    private float TimeOffCurrent;
+    void Start()
+    {
+        Spider.SetActive(false);
+        anim = GetComponent<Animator>();
+        Knight = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        TimeOffCurrent = TimeOf;
+    }
+
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, Knight.position) < 2.7)
+        {
+            anim.SetBool("CloseUp", true);
+            TimeOffCurrent -= Time.deltaTime;
+        }
+        if (TimeOffCurrent < 0)
+        {
+            Spider.SetActive(true);
+            anim.SetBool("CloseUp", false);
+        }
+    }
+}
