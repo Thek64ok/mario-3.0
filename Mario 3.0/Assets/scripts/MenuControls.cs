c8a6e7054e282fd50c5b041cdc3212483cc3b9f1
twixer32 <60601115+twixer32@users.noreply.github.com>
2020-02-15 11:35:13 +0200

    menu

---
+++ Mario 3.0/Assets/scripts/MenuControls.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuControls : MonoBehaviour
+{
+    public void PlayPressed()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ExitPressed()
+    {
+        Debug.Log("Exit pressed!");
+        Application.Quit();
+    }
+
+
+}
