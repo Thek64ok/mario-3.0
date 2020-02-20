b5211f96affa29fd74a6ad5d77fd8034f3c5ea24
Thek64ok <60596506+Thek64ok@users.noreply.github.com>
2020-02-20 20:41:54 +0200

    отталкивает в противоположную сторону

--- Mario 3.0/Assets/scripts/Knight_Hurt.cs
+++ Mario 3.0/Assets/scripts/Knight_Hurt.cs
@@ -8,15 +8,52 @@ public class Knight_Hurt : MonoBehaviour
     public int damageToGive;
     public bool checkDamage;
     public GameObject knight;
+    public GameObject pointR;
+    public GameObject pointL;
+    private pointRight rPoint;
+    private pointLeft lPoint;
     private wasd checkD;
+    private Vector3 dir;
+    public Transform pointRight;
+    public Transform pointLeft;
+    public Transform pointUp;
+    public Transform pointDown;
+
     void Start()
     {
         checkD = knight.GetComponent<wasd>();
+        rPoint = pointR.GetComponent<pointRight>();
+        lPoint = pointL.GetComponent<pointLeft>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (checkDamage)
+        {
+            if (rPoint.right == true)
+                checkD.myrigidbody2D.AddForce(Vector2.left * 5f, ForceMode2D.Impulse);
+            else
+            {
+                if (lPoint.left == true)
+                    checkD.myrigidbody2D.AddForce(Vector2.right * 5f, ForceMode2D.Impulse);
+            }
+            /*
+            if (Physics2D.OverlapPoint(pointRight.transform.position))
+                checkD.myrigidbody2D.AddForce(Vector2.left * 5f, ForceMode2D.Impulse);
+            else
+            {
+                if (Physics2D.OverlapPoint(pointLeft.transform.position))
+                    checkD.myrigidbody2D.AddForce(Vector2.right * 5f, ForceMode2D.Impulse);
+            }
+            */
+        }
+        /*
+        if (gameObject.transform.position.x + checkD.transform.position.x < 0)
+        if (checkDamage)
+            checkD.myrigidbody2D.AddForce(gameObject.transform.position /2f, ForceMode2D.Impulse);
+            */
+        /*
         if (checkDamage)
         {
             if (checkD.lastMove.x > 0)
@@ -34,6 +71,7 @@ public class Knight_Hurt : MonoBehaviour
                 }
             }
         }
+        */
     }
 
     void OnCollisionEnter2D(Collision2D other)
---
+++ Mario 3.0/Assets/scripts/points/pointRight.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pointRight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool right;
+    public GameObject knight;
+    private wasd dayn;
+    void Start()
+    {
+        dayn = knight.GetComponent<wasd>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (right)
+            dayn.myrigidbody2D.AddForce(Vector2.left * 5f, ForceMode2D.Impulse);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+            right = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+            right = false;
+    }
+}
