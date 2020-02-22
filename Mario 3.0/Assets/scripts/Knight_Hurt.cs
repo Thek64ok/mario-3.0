11e5c20ec83e420305d40d3a09e1c45d27018783
Thek64ok <60596506+Thek64ok@users.noreply.github.com>
2020-02-22 21:50:31 +0200

    доработаны коллайдеры

--- Mario 3.0/Assets/scripts/Knight_Hurt.cs
+++ Mario 3.0/Assets/scripts/Knight_Hurt.cs
@@ -10,20 +10,21 @@ public class Knight_Hurt : MonoBehaviour
     public GameObject knight;
     public GameObject pointR;
     public GameObject pointL;
+    public GameObject pointU;
+    public GameObject pointD;
     private pointRight rPoint;
     private pointLeft lPoint;
+    private pointUp uPoint;
+    private pointDown dPoint;
     private wasd checkD;
-    private Vector3 dir;
-    public Transform pointRight;
-    public Transform pointLeft;
-    public Transform pointUp;
-    public Transform pointDown;
 
     void Start()
     {
         checkD = knight.GetComponent<wasd>();
         rPoint = pointR.GetComponent<pointRight>();
         lPoint = pointL.GetComponent<pointLeft>();
+        uPoint = pointU.GetComponent<pointUp>();
+        dPoint = pointD.GetComponent<pointDown>();
     }
 
     // Update is called once per frame
@@ -32,11 +33,26 @@ public class Knight_Hurt : MonoBehaviour
         if (checkDamage)
         {
             if (rPoint.right == true)
-                checkD.myrigidbody2D.AddForce(Vector2.left * 5f, ForceMode2D.Impulse);
+                checkD.myrigidbody2D.AddForce(Vector2.left * 20f, ForceMode2D.Impulse);
             else
             {
                 if (lPoint.left == true)
-                    checkD.myrigidbody2D.AddForce(Vector2.right * 5f, ForceMode2D.Impulse);
+                    checkD.myrigidbody2D.AddForce(Vector2.right * 20f, ForceMode2D.Impulse);
+                else
+                {
+                    if (uPoint.up == true)
+                        checkD.myrigidbody2D.AddForce(Vector2.down * 20f, ForceMode2D.Impulse);
+                    else
+                    {
+                        if (dPoint.down == true)
+                            checkD.myrigidbody2D.AddForce(Vector2.up * 20f, ForceMode2D.Impulse);
+                        else
+                        {
+                            if (rPoint.right == true && lPoint.left)
+                                checkD.myrigidbody2D.AddForce(Vector2.right * 30f, ForceMode2D.Impulse);
+                        }
+                    }
+                }
             }
             /*
             if (Physics2D.OverlapPoint(pointRight.transform.position))
