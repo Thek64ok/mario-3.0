3a7569f909080a73b06fbc70946dcef5f8b3799d
Thek64ok <60596506+Thek64ok@users.noreply.github.com>
2020-02-27 19:22:58 +0200

    322

--- Mario 3.0/Assets/scripts/loadSkillTreeScen.cs
+++ Mario 3.0/Assets/scripts/loadSkillTreeScen.cs
@@ -2,19 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class loadSkillTreeScen : MonoBehaviour
 {
     private knightStats stats;
+    public Button button;
     void Start()
     {
-        stats = GetComponent<knightStats>();
+        stats = FindObjectOfType<knightStats>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (stats.skillPoints > 0)
-            SceneManager.LoadScene("Skill Tree");
+        if (stats.skillPoints == 0)
+            button.enabled = false;
+        else
+            if (stats.skillPoints > 0)
+            button.enabled = true;
+    }
+
+    public void PlayPressed()
+    { 
+        SceneManager.LoadScene("Skill Tree");
     }
 }
