7534591fe8047b4ec0d1deb36711663201de6969
Thek64ok <60596506+Thek64ok@users.noreply.github.com>
2020-02-27 19:09:30 +0200

    yura die

---
+++ Mario 3.0/Assets/scripts/loadSkillTreeScen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadSkillTreeScen : MonoBehaviour
+{
+    private knightStats stats;
+    void Start()
+    {
+        stats = GetComponent<knightStats>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (stats.skillPoints > 0)
+            SceneManager.LoadScene("Skill Tree");
+    }
+}
