abaeaef05873a02f48762fec0839aa5bf961a846
karadjayura123 <60612656+karadjayura123@users.noreply.github.com>
2020-03-08 12:19:09 +0200

    Zalupa
    Zalupa

---
+++ Mario 3.0/Assets/scripts/Dialog_NextClick.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialog_NextClick : MonoBehaviour
+{
+    public GameObject Text1;
+    public GameObject Text2;
+    public bool isText1 = true;
+    public NPC_Task npc_taskScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { if(Input.GetMouseButtonDown(0))
+                { if (isText1 == true)
+            {
+                isText1 = false;
+            }
+            else
+            {
+                isText1 = true;
+                npc_taskScript.EndDialog = true;
+            }
+                }
+        if(isText1 == true)
+                {
+            Text1.SetActive(true);
+            Text2.SetActive(false);
+
+                }else
+            {
+            Text1.SetActive(false);
+                Text2.SetActive(true);
+                }
+
+
+    }
+}
---
+++ Mario 3.0/Assets/scripts/NPC_Task.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Task : MonoBehaviour
+{
+    public bool EndDialog;
+    public GameObject Dialog1;
+    public Quest_Event EE;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (EndDialog == true)
+        {
+            Time.timeScale = 1;
+            EE.Quest1 = true;
+            Dialog1.SetActive(false);
+        }
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            Time.timeScale = 0;
+            Dialog1.SetActive(true);
+        }
+    }
+}
+
