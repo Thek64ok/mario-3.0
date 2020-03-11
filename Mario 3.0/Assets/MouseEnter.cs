ca3d3fc748676ca4d8eeea383db640a52dd7e49b
Thek64ok <60596506+Thek64ok@users.noreply.github.com>
2020-03-11 14:06:39 +0200

    gui для гнид

--- Mario 3.0/Assets/MouseEnter.cs
+++ Mario 3.0/Assets/MouseEnter.cs
@@ -5,20 +5,10 @@ using UnityEngine.UI;
 
 public class MouseEnter : MonoBehaviour
 {
-    public Text[] text;
     public GameObject trap;
     void Start()
     {
         trap.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        text[0].text = "Путь Мечника";
-        text[1].text = "Стоимость: 1 очко навыка";
-        text[2].text = "Меч наделяется благословением солнца, что даёт прибавку к урону против нежити +20% от текущего урона меча";
-        text[3].text = "Что может быть лучше побежденной нежити?\n " + " - спросили рыцаря\n" + " Две побежденные нежити";
     }
     public void OnMouseEnter()
     {
