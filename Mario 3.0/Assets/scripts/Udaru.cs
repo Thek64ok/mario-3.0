c427b243ebab129b945c9d2c0900ca350d4aed8c
karadjayura123 <60612656+karadjayura123@users.noreply.github.com>
2020-03-21 12:07:09 +0200

    2
    2

---
+++ Mario 3.0/Assets/scripts/Udaru.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Udaru : MonoBehaviour
+{
+
+    public AudioClip footsteps;
+
+
+    public void Zalupa ()
+    {
+
+        AudioSource.PlayClipAtPoint(footsteps, transform.position);
+    }
+}
