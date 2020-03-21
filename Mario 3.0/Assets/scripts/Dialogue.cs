ce4e33404f7d59ed3349c3832384bb0e2660e191
Thek64ok <60596506+Thek64ok@users.noreply.github.com>
2020-03-21 17:17:21 +0200

    диалоги

--- Mario 3.0/Assets/scripts/Dialogue.cs
+++ Mario 3.0/Assets/scripts/Dialogue.cs
@@ -7,32 +7,35 @@ public class Dialogue : MonoBehaviour
     public DialogueNode[] node;
     public int _currentNode;
     public bool ShowDialogue = true;
+    private textFor ins;
+    void Start()
+    {
+        ins = FindObjectOfType<textFor>();
+    }
+
+    void Update()
+    {
+        ShowDialogue = true;
+    }
 
     void OnGUI()
     {
-
-
-
+        Time.timeScale = 0;
         if (ShowDialogue == true)
         {
-            if (Input.GetKeyDown(KeyCode.E))
+            GUI.Box(new Rect(Screen.width / 2 - 300, Screen.height - 300, 600, 250), "");
+            GUI.Label(new Rect(Screen.width / 2 - 250, Screen.height - 280, 500, 90), node[_currentNode].NpcText);
+            for (int i = 0; i < node[_currentNode].PlayerAnswer.Length; i++)
             {
-                Time.timeScale = 0.00000000001f;
-                if (ShowDialogue == true)
+                if (GUI.Button(new Rect(Screen.width / 2 - 250, Screen.height - 200 + 25 * i, 500, 25), node[_currentNode].PlayerAnswer[i].Text))
                 {
-                    GUI.Box(new Rect(Screen.width / 2 - 300, Screen.height - 300, 600, 250), "");
-                    GUI.Label(new Rect(Screen.width / 2 - 250, Screen.height - 280, 500, 90), node[_currentNode].NpcText);
-                    for (int i = 0; i < node[_currentNode].PlayerAnswer.Length; i++)
+                    if (node[_currentNode].PlayerAnswer[i].SpeakEnd)
                     {
-                        if (GUI.Button(new Rect(Screen.width / 2 - 250, Screen.height - 200 + 25 * i, 500, 25), node[_currentNode].PlayerAnswer[i].Text))
-                        {
-                            if (node[_currentNode].PlayerAnswer[i].SpeakEnd)
-                            {
-                                ShowDialogue = false;
-                            }
-                            _currentNode = node[_currentNode].PlayerAnswer[i].ToNode;
-                        }
+                        ShowDialogue = false;
+                        Time.timeScale = 1f;
+                        ins.inside = false;
                     }
+                    _currentNode = node[_currentNode].PlayerAnswer[i].ToNode;
                 }
             }
         }
---
+++ Mario 3.0/Assets/scripts/textFor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class textFor : MonoBehaviour
+{
+    public bool inside;
+    public GameObject dialog;
+    void Start()
+    {
+        inside = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inside)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+                dialog.SetActive(true);
+        }
+        else
+            dialog.SetActive(false);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Knight")
+            inside = true;
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Knight")
+            inside = false;
+    }
+}
