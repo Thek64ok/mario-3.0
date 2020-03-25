51f40151196f9dcd60d3956ec4ffceeeeedbef45
karadjayura123 <60612656+karadjayura123@users.noreply.github.com>
2020-03-25 17:09:26 +0200

    кВЕСТЫ

---
+++ Mario 3.0/Assets/scripts/Cube.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public bool dolboeb;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+   
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            dolboeb = true;
+            if (PlayerPrefs.GetInt("Quest1") == 1)
+            {
+                PlayerPrefs.SetInt("Quest1", 2);
+            }
+        }
+    }
+}
