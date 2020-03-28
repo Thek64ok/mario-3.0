41da6d7df1387c588623ee129f59a181b5134e4b
Thek64ok <60596506+Thek64ok@users.noreply.github.com>
2020-03-28 10:29:54 +0200

    222

--- Mario 3.0/Assets/scripts/MenuControls.cs
+++ Mario 3.0/Assets/scripts/MenuControls.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 
@@ -8,10 +9,30 @@ public class MenuControls : MonoBehaviour
 {
     public GameObject loadPanel;
     public GameObject mainPanel;
+    public Text textInFirstSlot;
+    public Text textInSecondSlot;
+    public Text textInThirdSlot;
+    public Text textInFourthSlot;
+    public Text textInFivethSlot;
     private PauseMenu load;
     void Start()
     {
         load = FindObjectOfType<PauseMenu>();
+        textInFirstSlot.text = PlayerPrefs.GetString("FirstFile");
+        textInSecondSlot.text = PlayerPrefs.GetString("SecondFile");
+        textInThirdSlot.text = PlayerPrefs.GetString("ThithFile");
+        textInFourthSlot.text = PlayerPrefs.GetString("FourthFile");
+        textInFivethSlot.text = PlayerPrefs.GetString("FivethFile");
+    }
+    private void Update()
+    {
+        /*
+        textInFirstSlot.text = PlayerPrefs.GetString("FirstFile");
+        textInSecondSlot.text = PlayerPrefs.GetString("SecondFile");
+        textInThirdSlot.text = PlayerPrefs.GetString("ThithFile");
+        textInFourthSlot.text = PlayerPrefs.GetString("FourthFile");
+        textInFivethSlot.text = PlayerPrefs.GetString("FivethFile");
+        */
     }
     public void PlayPressed()
     {
